373c229cc3a56bdfd82f269dd9ae75c2e8e91082
myanony1 <54415395+myanony1@users.noreply.github.com>
2025-01-06 16:14:19 +0300

    Update update_urls.py

--- update_urls.py
+++ update_urls.py
@@ -22,29 +22,39 @@ if links:
     final_url = redirect_response.url
     
     # .m3u8 URL'sini ve referer URL'sini çıkart
-    m3u8_url = final_url.split(' ')[0]  # Eğer URL içerisinde farklı parametreler varsa bu kısmı tekrar kontrol edin
-    referer_url = final_url.split(' ')[1]  # Eğer URL içerisinde farklı parametreler varsa bu kısmı tekrar kontrol edin
+    m3u8_url, referer_url = final_url.split(' ')[0], final_url.split(' ')[1]
 
-    # Şimdi, index.html dosyasındaki exotrgoals sınıfında bunları güncelle
+    # Şimdi, index.html dosyasındaki exotrgoals1 sınıfında bunları güncelle
     index_url = "https://raw.githubusercontent.com/myanony1/myApp/main/.index.html"
     index_response = requests.get(index_url)
     index_html = index_response.text
 
     # HTML içeriğini güncelle
-    updated_html = index_html.replace("https://k0.b4c8d3e9f1a2b7c5d5.cfd/yayinzirve.m3u8", m3u8_url)
-    updated_html = updated_html.replace("https://trgoals1097.xyz", referer_url)
+    from bs4 import BeautifulSoup
 
-    # Güncellenmiş HTML dosyasını geri göndermek için commit yap
-    with open(".index.html", "w") as file:
-        file.write(updated_html)
+    soup = BeautifulSoup(index_html, "html.parser")
+    exotrgoals_tag = soup.find(class_="exotrgoals1")
 
-    # Git işlemleri
-    import subprocess
-    subprocess.run(["git", "config", "--global", "user.name", "ActionBot"])
-    subprocess.run(["git", "config", "--global", "user.email", "actionbot@example.com"])
-    subprocess.run(["git", "add", ".index.html"])
-    subprocess.run(["git", "commit", "-m", "Updated m3u8 and referer URLs"])
-    subprocess.run(["git", "push"])
+    if exotrgoals_tag:
+        # .m3u8 URL'sini ve referer URL'sini güncelle
+        exotrgoals_tag.string = exotrgoals_tag.string.replace("https://d0.b4c8d3e9f1a2b7c5d5.cfd/yayinzirve.m3u8", m3u8_url)
+        exotrgoals_tag.string = exotrgoals_tag.string.replace("https://trgoals1094.xyz", referer_url)
+        
+        # Güncellenmiş HTML içeriği
+        updated_html = soup.prettify()
 
+        # Güncellenmiş HTML dosyasını kaydet
+        with open(".index.html", "w") as file:
+            file.write(updated_html)
+
+        # Git işlemleri
+        import subprocess
+        subprocess.run(["git", "config", "--global", "user.name", "ActionBot"])
+        subprocess.run(["git", "config", "--global", "user.email", "actionbot@example.com"])
+        subprocess.run(["git", "add", ".index.html"])
+        subprocess.run(["git", "commit", "-m", "Updated m3u8 and referer URLs"])
+        subprocess.run(["git", "push"])
+    else:
+        print("exotrgoals1 sınıfı bulunamadı.")
 else:
     print("No links found.")
