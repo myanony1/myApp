815b6d8648f8be570ee84cc4f662dd268d624f2b
myanony1 <54415395+myanony1@users.noreply.github.com>
2025-01-07 12:45:39 +0300

    Update update-url.py

--- update-url.py
+++ update-url.py
@@ -12,16 +12,16 @@ m3u_content = response.text
 print("M3U Dosyasının İçeriği:")
 print(m3u_content)
 
-# trgoals içeren URL'leri bulmak için daha esnek bir regex kullanıyoruz
-m3u_urls = re.findall(r'https?://[^\s]+trgoals[^\s]*', m3u_content)
+# trgoals içeren http-referrer URL'lerini bulmak için regex kullanıyoruz
+m3u_referrer_urls = re.findall(r'#EXTVLCOPT:http-referrer=https?://[^\s]+trgoals[^\s]*', m3u_content)
 
 # M3U dosyasındaki bulunan URL'leri yazdır
-if m3u_urls:
-    print("\ntrgoals içeren URL'ler:")
-    for url in m3u_urls:
+if m3u_referrer_urls:
+    print("\ntrgoals içeren http-referrer URL'leri:")
+    for url in m3u_referrer_urls:
         print(url)
 else:
-    print("trgoals içeren URL bulunamadı.")
+    print("trgoals içeren http-referrer URL bulunamadı.")
 
 # index.html dosyasını aç ve URL'leri değiştir
 index_file_path = '.index.html'
@@ -30,11 +30,11 @@ with open(index_file_path, 'r') as file:
     html_content = file.read()
 
 # .index.html dosyasındaki trgoals içeren URL'leri bul
-html_urls = re.findall(r'https?://[^\s]+trgoals[^\s]*', html_content)
+html_referrer_urls = re.findall(r'https?://[^\s]+trgoals[^\s]*', html_content)
 
 # Eğer trgoals içeren URL'ler varsa, bunları M3U dosyasındaki URL'lerle değiştir
-if html_urls:
-    for old_url, new_url in zip(html_urls, m3u_urls):
+if html_referrer_urls:
+    for old_url, new_url in zip(html_referrer_urls, m3u_referrer_urls):
         html_content = html_content.replace(old_url, new_url)
 
     # Değiştirilen içeriği tekrar index.html dosyasına yaz
@@ -43,4 +43,4 @@ if html_urls:
 
     print("URL güncellemesi başarıyla yapıldı.")
 else:
-    print("trgoals içeren URL'ler bulunamadı.")
+    print("trgoals içeren http-referrer URL'ler bulunamadı.")
