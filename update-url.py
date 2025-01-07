2c252f7b01480f0a712eebd5847c9ffff3e445fa
myanony1 <54415395+myanony1@users.noreply.github.com>
2025-01-07 12:40:57 +0300

    Update update-url.py

--- update-url.py
+++ update-url.py
@@ -1,23 +1,34 @@
 import requests
+import re
 
-# M3U dosyasındaki URL'yi çek
+# M3U dosyasındaki URL'leri çek
 m3u_url = 'https://raw.githubusercontent.com/keyiflerolsun/IPTV_YenirMi/main/Kanallar/KekikAkademi.m3u'
 response = requests.get(m3u_url)
 
-# URL'yi al
-new_url = response.text.strip()  # Eğer m3u dosyasındaki URL metinse, bunu alıyoruz
+# M3U dosyasındaki URL'leri al
+m3u_content = response.text
 
-# index.html dosyasını aç ve URL'yi değiştir
-index_file_path = '.index.html'
+# M3U dosyasındaki trgoals içeren URL'leri bul
+m3u_urls = re.findall(r'https?://[^\s]+trgoals[^\s]+', m3u_content)
+
+# index.html dosyasını aç ve URL'leri değiştir
+index_file_path = 'index.html'
 
 with open(index_file_path, 'r') as file:
     html_content = file.read()
 
-# trgoals içeren URL'yi değiştirme
-updated_html = html_content.replace('trgoals', new_url)
+# .index.html dosyasındaki trgoals içeren URL'leri bul
+html_urls = re.findall(r'https?://[^\s]+trgoals[^\s]+', html_content)
 
-# Değiştirilen içeriği tekrar index.html dosyasına yaz
-with open(index_file_path, 'w') as file:
-    file.write(updated_html)
+# Eğer trgoals içeren URL'ler varsa, bunları M3U dosyasındaki URL'lerle değiştir
+if html_urls:
+    for old_url, new_url in zip(html_urls, m3u_urls):
+        html_content = html_content.replace(old_url, new_url)
 
-print("URL güncellemesi başarıyla yapıldı.")
+    # Değiştirilen içeriği tekrar index.html dosyasına yaz
+    with open(index_file_path, 'w') as file:
+        file.write(html_content)
+
+    print("URL güncellemesi başarıyla yapıldı.")
+else:
+    print("trgoals içeren URL'ler bulunamadı.")
